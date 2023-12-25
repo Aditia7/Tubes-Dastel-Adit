ea7b8046a4793047c7e02c723d9fc0d9030f98aa
Aditia7 <154493592+Aditia7@users.noreply.github.com>
2023-12-25 16:43:00 +0700

    Update and rename Codingan to Codingan.py

--- Codingan.py
+++ Codingan.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 with st.sidebar :
     st.image("download.png")
-    selected = option_menu ("Program dibuat oleh Aditia Nugraha Nrp 11-20190-067 Mahasiswa ITENAS.\n      Tugas Dasar Telekomunikasi.\n             Dosen Pengampu : Ir. Rustamanji, M.T.\t  ",
+    selected = option_menu ("Program dibuat oleh Aditia Nugraha Nrp 11-2019-067 Mahasiswa ITENAS.\n      Tugas Dasar Telekomunikasi.\n             Dosen Pengampu : Ir. Rustamanji, M.T.\t  ",
     ["Perhitungan Impedansi Karakteristik",
     "Perhitungan Konstanta Propagasi",
     "Perhitungan Rasio Daya",
