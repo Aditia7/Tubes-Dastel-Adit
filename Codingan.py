58db769c17b7b7ac5cd1185bef9223d23529b544
Aditia7 <154493592+Aditia7@users.noreply.github.com>
2024-01-03 17:47:59 +0700

    Update Codingan.py

--- Codingan.py
+++ Codingan.py
@@ -6,13 +6,52 @@ from PIL import Image
 with st.sidebar :
     st.image("download.png")
     selected = option_menu ("Program dibuat oleh Aditia Nugraha NRP 11-2019-067 Mahasiswa ITENAS.\n      Tugas Dasar Telekomunikasi.\n             Dosen Pengampu : Ir. Rustamanji, M.T.\t  ",
-    ["Perhitungan Rasio Daya",
+    ["Perhitungan Impedansi Karakteristik",
+    "Perhitungan Konstanta Propagasi",
+    "Perhitungan Rasio Daya",
     "Perhitungan Rasio Tegangan",
     "Perhitungan Rasio Arus",
     "Perhitungan Rasio Penguatan",
     "Perhitungan Rasio Redaman",],
     default_index=0)
+
+if(selected == "Perhitungan Impedansi Karakteristik") :
+    st.title("Rangkaian Ekivalen Saluran Transmisi")
+    st.image("saluran1.jpg")
+    st.title("Rumus Impedansi Karakteristik")
+    st.image("Impedansi1.jpg")
+    st.title("Perhitungan Impedansi Karakteristik")
+
+    r = st.number_input ("masukan nilai Resistor(ohm/km)", 0.000)
+    l= st.number_input ("masukan nilai Induktor(henry/km)", 0.000)
+    c= st.number_input ("masukan nilai Kapasitor(farad/km)", 0.000)
+    g= st.number_input ("masukan nilai Konduktansi(siemens/km)", 0.000)
+    f= st.number_input ("masukan nilai frekuensi(Hz)", 0)
+    hitung = st.button ("hitung Impedansi Karakteristik")
+
+    if hitung : 
+        Z_Propagasi=(((r+complex(0,2*math.pi*f*l))/(g+complex(0,2*math.pi*f*c)))**0.5)
+        st.write("nilai Impedansi Karakteristik adalah = ", Z_Propagasi)
+        st.success(f"nilai Impedansi Karakteristik adalah = {Z_Propagasi} ohm")
     
+if(selected == "Perhitungan Konstanta Propagasi") :
+    st.title("Rangkaian Ekivalen Saluran Transmisi")
+    st.image("saluran1.jpg")
+    st.title("Rumus Konstanta Propagasi")
+    st.image("konstanta1.jpg")
+    st.title("Perhitungan Konstanta Propagasi")
+
+    r = st.number_input ("masukan nilai Resistor(ohm/km)", 0.000)
+    l= st.number_input ("masukan nilai Induktor(henry/km)", 0.000)
+    c= st.number_input ("masukan nilai Kapasitor(farad/km)", 0.000)
+    g= st.number_input ("masukan nilai Konduktansi(siemens/km)", 0.000)
+    f= st.number_input ("masukan nilai frekuensi(Hz)", 0)
+    hitung = st.button ("hitung Konstanta Progagasi")
+
+    if hitung : 
+        K_Propagasi=(((r+complex(0,2*math.pi*f*l))*(g+complex(0,2*math.pi*f*c)))**0.5)
+        st.write("nilai konstanta propagasi adalah = ", K_Propagasi)
+        st.success(f"nilai konstanta propagasi adalah = {K_Propagasi}")
         
 if(selected == "Perhitungan Rasio Daya") :
     st.title("Rumus Rasio Daya")
