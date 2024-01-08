8c30299b5a008b976cba841ed967226c5157689a
Aditia7 <154493592+Aditia7@users.noreply.github.com>
2024-01-08 09:52:38 +0700

    Update Iman.py

--- Iman.py
+++ Iman.py
@@ -31,7 +31,7 @@ if(selected == "Perhitungan Impedansi Karakteristik") :
     if hitung : 
         Z_Propagasi=(((r+complex(0,2*math.pi*f*l))/(g+complex(0,2*math.pi*f*c)))**0.5)
         b = math.degrees(math.atan((g+(0,2*math.pi*f*c)))/(r+(0,2*math.pi*f*l)))
-        a = ((r+(0,2*math.pi*f*l))+(g+(0,2*math.pi*f*c)))**0.5
+        a = (((r+(0,2*math.pi*f*l))+(g+(0,2*math.pi*f*c)))**0.5)
         st.write("nilai Impedansi Karakteristik adalah = ", Z_Propagasi)
         st.write("nilai Impedansi Karakteristik adalah = ", a)
         st.write("nilai Impedansi Karakteristik adalah = °", b)
@@ -55,7 +55,7 @@ if(selected == "Perhitungan Konstanta Propagasi") :
     if hitung : 
         K_Propagasi=(((r+complex(0,2*math.pi*f*l))*(g+complex(0,2*math.pi*f*c)))**0.5)
         b = math.degrees(math.atan((g+(0,2*math.pi*f*c)))/(r+(0,2*math.pi*f*l)))
-        a = ((r+(0,2*math.pi*f*l))+(g+(0,2*math.pi*f*c)))**0.5
+        a =(((r+(0,2*math.pi*f*l))+(g+(0,2*math.pi*f*c)))**0.5)
         st.write("nilai konstanta propagasi adalah = ", K_Propagasi)
         st.success(f"nilai konstanta propagasi adalah = {K_Propagasi}")
         st.write("nilai Impedansi Karakteristik adalah = ", a)
