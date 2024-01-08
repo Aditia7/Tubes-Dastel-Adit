de35cc56a9261bec98077fdc334a04b859b90320
Aditia7 <154493592+Aditia7@users.noreply.github.com>
2024-01-08 12:09:59 +0700

    Update Codingan.py

--- Codingan.py
+++ Codingan.py
@@ -37,7 +37,7 @@ if(selected == "Perhitungan Impedansi Karakteristik") :
         st.write("nilai Magnitude Impedansi Karakteristik nya = ohm", a)
         st.write("nilai Sudut Impedansi Karakteristik nya = °", b)
         st.success(f"nilai Impedansi Karakteristik adalah = {Z_Propagasi} ohm")
-        st.success(f"nilai Impedansi Karakteristik adalah = {a} ∠ {b}°")
+        st.success(f"nilai Impedansi Karakteristik adalah = {a} ∠ {b}° ohm")
     
 if(selected == "Perhitungan Konstanta Propagasi") :
     st.title("Rangkaian Ekivalen Saluran Transmisi")
